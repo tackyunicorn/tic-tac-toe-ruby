fdbbd8fb507aa5afc578464dd09f55dcabfa2758
tackyunicorn <joshjoseph97@gmail.com>
2019-03-08 10:54:55 +0900

    :page_with_curl: adds documentation and closes #2

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,12 +1,18 @@
 class TicTacToe
+
+	# The board is a Hash of spots (keys) and their corresponding values
+	# (empty for now)
 	@@board = { 7 => ' ' , 8 => ' ' , 9 => ' ' ,
 				4 => ' ' , 5 => ' ' , 6 => ' ' ,
 				1 => ' ' , 2 => ' ' , 3 => ' '  }
 
+	# wins is a list containing all possible configurations of the
+	# board in the winning state
 	@@wins = [  [7 , 4 , 1] , [8 , 5 , 2] , [9 , 6 , 3] ,
 				[7 , 8 , 9] , [4 , 5 , 6] , [1 , 2 , 3] ,
 				[7 , 5 , 3] , [1 , 5 , 9]  ]
 
+	# show is a function that displays the current state of the board
 	def show()
 		puts (' 	|	' + ' 	|	')
 		puts (@@board[7] + '	|	' + @@board[8] + '	|	' + @@board[9])
@@ -18,12 +24,16 @@ class TicTacToe
 		puts (' 	|	' + ' 	|	')
 	end
 
+	# checkline is used as a helper function for checkall that returns the
+	# the move if all the spots in a line have the same move
 	def checkline(move , spot1 , spot2 , spot3)
 		if @@board[spot1] == move && @@board[spot2] == move && @@board[spot3] == move then
 			return move
 		end
 	end
 
+	# checkall checks the board for all possible win configurations on a 
+	# particular move and returns true if a configuration is found
 	def checkall(move)
 		(0...@@wins.length).each do |i|
 			if checkline(move , @@wins[i][0] , @@wins[i][1] , @@wins[i][2]) == move then
@@ -33,6 +43,8 @@ class TicTacToe
 		return false
 	end
 
+	# turn gives the turn to a particular player, makes the player
+	# select a spot and calls checkall to see if the player has won
 	def turn(player)
 		system "cls"
 		puts ("<=============PLAYER #{player}=============>")
@@ -61,32 +73,41 @@ class TicTacToe
 		end
 	end
 
+	# game starts the game and keeps a counter of spots taken
+	# to check for a tie condition. returns 0 on a tie, 1 on
+	# player 1 winning and 2 on player 2 winning
 	def game()
 		@taken = 0
-		
+
 		while true do
 
 			if (@taken > 7) then
-				puts ('The game ends in a tie')
-				break
+				return 0
 			end
-			
+
 			@player = 1
 			if turn(@player) then
-				puts ("Player #{@player} wins!")
-				break
+				return 1
 			end
+
 			@player = 2
 			if turn(@player) then
-				puts ("Player #{@player} wins!")
-				break
+				return 2
 			end
 
 		end
 	end
 end
 
+# create a class object for TicTacToe and call the game method
 system "cls"
 game = TicTacToe.new
 result = game.game
-puts (result)
+
+if result == 0 then
+	puts ('The game ends in a tie')
+elsif result == 1
+	puts ("Player 1 wins!")
+else
+	puts ("Player 2 wins!")
+end
