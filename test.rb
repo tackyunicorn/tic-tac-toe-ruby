f033c5fe363a476e4218fd6b6d988f66a37a4674
tackyunicorn <joshjoseph97@gmail.com>
2019-03-11 11:04:24 +0900

    :flashlight: generalizing board, wins, show

--- test.rb
+++ test.rb
@@ -1,29 +1,66 @@
 require 'Matrix'
 
 class Matrix
-	def extract_diagonal
-		each(:diagonal).to_a
-	end
-	def rotate
-		Matrix[*to_a.map(&:reverse).transpose]
-	end
+    def extract_diagonal
+        each(:diagonal).to_a
+    end
+    def rotate
+        Matrix[*to_a.map(&:reverse).transpose]
+    end
 end
 
-@size = 3
+class Test
 
-@spot = 0
+  @@spot = 0
+  @@size = 3
 
-@x = Matrix.build(@size) {|row , col| @spot += 1}
+  @@x = Matrix.build(@@size) {|row , col| @@spot += 1}
 
-@wins = Array.new()
+  @@board = {}
+  (1..@@size**2).each {|i| @@board[i] = ' '}
 
-@win_vectors =  @x.row_vectors.each { |sub| sub.each { |list| list } }
+  @@temp = []
 
-@wins.push(@x.extract_diagonal)
-@wins.push(@x.rotate.extract_diagonal)
-@wins.push(@x.row_vectors.each {|sub| sub.to_a})
-@wins.push(@x.column_vectors.each {|sub| sub.to_a})
+  @@wins = Array.new()
+  @@x.row_vectors.each do |sub|
+    @@temp.push(sub.to_a)
+  end
+  @@temp.reverse.each { |x| @@wins.push(x) }
+  @@x.column_vectors.each do |sub|
+    @@wins.push(sub.to_a)
+  end
+  @@wins.push(@@x.extract_diagonal)
+  @@wins.push(@@x.rotate.extract_diagonal)
 
-print (@wins)
-puts ()
-print (@win_vectors)
+  def test
+    (1..@@size**2).each {|i| @@board[i] = ' '}
+    # print @@board
+    print @@wins
+  end
+
+  def show
+    (0...@@size).each do |i|
+      if i == 0 then
+        (0..@@size).each do
+          print ('----')
+        end
+        puts ()
+      end
+      (0...@@size).each do |j|
+        if j == 0 then
+          print ('| ' + @@board[@@wins[i][j]] + ' |')
+        else
+          print ('  ' + @@board[@@wins[i][j]] + ' |')
+        end
+      end
+      puts ()
+      (0..@@size).each do
+        print ('----')
+      end
+      puts()
+    end
+  end
+end
+
+a = Test.new
+puts a.show
--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,129 +1,167 @@
+require 'Matrix'
+
+class Matrix
+    def extract_diagonal
+        each(:diagonal).to_a
+    end
+    def rotate
+        Matrix[*to_a.map(&:reverse).transpose]
+    end
+end
+
 class TicTacToe
 
-	# tic-tac-toe board is assumed to be of size 3 x 3
-	@@size = 3
-	# The board is a Hash of spots (keys) and their corresponding values
-	# (empty for now)
-	@@board = { 7 => ' ' , 8 => ' ' , 9 => ' ' ,
-				4 => ' ' , 5 => ' ' , 6 => ' ' ,
-				1 => ' ' , 2 => ' ' , 3 => ' '  }
+    # set the default size to be 3 x 3
+    @@size = 3
 
-	# wins is a list containing all possible configurations of the
-	# board in the winning state
-	@@wins = [  [7 , 4 , 1] , [8 , 5 , 2] , [9 , 6 , 3] ,
-				[7 , 8 , 9] , [4 , 5 , 6] , [1 , 2 , 3] ,
-				[7 , 5 , 3] , [1 , 5 , 9]  ]
+    # used to initialize the spots for the win configurations
+    @@spot = 0
 
-	# show is a function that displays the current state of the board
-	def show()
-		puts (' 	|	' + ' 	|	')
-		puts (@@board[7] + '	|	' + @@board[8] + '	|	' + @@board[9])
-		puts ('------------------------------------')
-		puts (@@board[4] + '	|	' + @@board[5] + '	|	' + @@board[6])
-		puts (' 	|	' + ' 	|	')
-		puts ('------------------------------------')
-		puts (@@board[1] + '	|	' + @@board[2] + '	|	' + @@board[3])
-		puts (' 	|	' + ' 	|	')
-	end
+    # build a matrix of the specified size, initialize it with
+    # values of the spots, (1..size**2)
+    @@x = Matrix.build(@@size) {|row , col| @@spot += 1}
 
-	# checkline is used as a helper function for check_all that returns
-	# the move if all the spots in a line have the same move
-	def checkline(move , spot1 , spot2 , spot3)
-		if @@board[spot1] == move && @@board[spot2] == move && @@board[spot3] == move then
-			return move
-		end
-	end
+    # The board is a Hash of spots (keys) and their corresponding values
+    # (empty for now)
+    @@board = {}
+    (1..@@size**2).each {|i| @@board[i] = ' '}
 
-	# check_all checks the board for all possible win configurations on a 
-	# particular move and returns true if a configuration is found
-	def check_all(move)
-		(0...@@wins.length).each do |i|
-			if checkline(move , @@wins[i][0] , @@wins[i][1] , @@wins[i][2]) == move then
-				return true
-			end
-		end
-		return false
-	end
+    # wins is a list containing all possible configurations of the
+    # board in the winning state
+    @@wins = Array.new()
+    @@temp = Array.new()
+    @@x.row_vectors.each do |sub|
+        @@temp.push(sub.to_a)
+    end
+    @@temp.reverse.each { |x| @@wins.push(x) }
+    @@x.column_vectors.each do |sub|
+        @@wins.push(sub.to_a)
+    end
+    @@wins.push(@@x.extract_diagonal)
+    @@wins.push(@@x.rotate.extract_diagonal)
 
-	# turn gives the turn to a particular player, makes the player
-	# select a spot and calls check_all to see if the player has won
-	def turn(player)
-		system "cls"
-		puts ("<=============PLAYER #{player}=============>")
-		puts ()
-		show
-		puts ()
-		puts ("Select a position [numpad layout]: ")
-		@point = gets.chomp.to_i
-		if (1..9).include?(@point) then
-			if (@@board[@point] != 'X' && @@board[@point] != 'O') then
-				if player == 1 then
-					@mark = 'X'
-				else
-					@mark = 'O'
-				end
+    # show is a function that displays the current state of the board
+    def show()
+        (0...@@size).each do |i|
+            if i == 0 then
+              (0..@@size).each do
+                print ('----')
+              end
+              puts ()
+            end
+            (0...@@size).each do |j|
+              if j == 0 then
+                print ('| ' + @@board[@@wins[i][j]] + ' |')
+              else
+                print ('  ' + @@board[@@wins[i][j]] + ' |')
+              end
+            end
+            puts ()
+            (0..@@size).each do
+              print ('----')
+            end
+            puts()
+        end
+    end
 
-				@@board[@point] = @mark
-				@taken += 1
-				if (check_all(@mark)) then
-					return true
-				else
-					return false
-				end
+    # checkline is used as a helper function for check_all that returns
+    # the move if all the spots in a line have the same move
+    def checkline(move , spot1 , spot2 , spot3)
+        if @@board[spot1] == move && @@board[spot2] == move && @@board[spot3] == move then
+            return move
+        end
+    end
 
-			else
-				puts ("That spot is taken!")
-				sleep(1)
-				turn(@player)
-			end
-		else
-			puts ("Invalid input")
-			sleep(1)
-			turn(@player)
-		end
-	end
+    # check_all checks the board for all possible win configurations on a 
+    # particular move and returns true if a configuration is found
+    def check_all(move)
+        (0...@@wins.length).each do |i|
+            if checkline(move , @@wins[i][0] , @@wins[i][1] , @@wins[i][2]) == move then
+                return true
+            end
+        end
+        return false
+    end
 
-	# game starts the game and keeps a counter of spots taken
-	# to check for a tie condition. returns 0 on a tie, 1 on
-	# player 1 winning and 2 on player 2 winning
-	def game()
-		@taken = 0
+    # turn gives the turn to a particular player, makes the player
+    # select a spot and calls check_all to see if the player has won
+    def turn(player)
+        system "cls"
+        puts ("<=============PLAYER #{player}=============>")
+        puts ()
+        show
+        puts ()
+        puts ("Select a position [numpad layout]: ")
+        @point = gets.chomp.to_i
+        if (1..9).include?(@point) then
+            if (@@board[@point] != 'X' && @@board[@point] != 'O') then
+                if player == 1 then
+                    @mark = 'X'
+                else
+                    @mark = 'O'
+                end
 
-		while true do
+                @@board[@point] = @mark
+                @taken += 1
+                if (check_all(@mark)) then
+                    return true
+                else
+                    return false
+                end
 
-			if (@taken > 7) then
-				system "cls"
-				puts ("<=============PLAYER #{@player}=============>")
-				puts ()
-				show
-				puts ()
-				return 0
-			end
+            else
+                puts ("That spot is taken!")
+                sleep(1)
+                turn(@player)
+            end
+        else
+            puts ("Invalid input")
+            sleep(1)
+            turn(@player)
+        end
+    end
 
-			# player 1 is assumed to take X and player 2 assumed to take Y
-			# this setting can be changed in the turn() method
-			@player = 1
-			if turn(@player) then
-				system "cls"
-				puts ("<=============PLAYER #{@player}=============>")
-				puts ()
-				show
-				puts ()
-				return 1
-			end
+    # game starts the game and keeps a counter of spots taken
+    # to check for a tie condition. returns 0 on a tie, 1 on
+    # player 1 winning and 2 on player 2 winning
+    def game()
+        @taken = 0
 
-			@player = 2
-			if turn(@player) then
-				system "cls"
-				puts ("<=============PLAYER #{@player}=============>")
-				puts ()
-				show
-				puts ()
-				return 2
-			end
+        while true do
 
-		end
-	end
+            if (@taken > 7) then
+                system "cls"
+                puts ("<=============PLAYER #{@player}=============>")
+                puts ()
+                show
+                puts ()
+                return 0
+            end
+
+            # player 1 is assumed to take X and player 2 assumed to take Y
+            # this setting can be changed in the turn() method
+            @player = 1
+            if turn(@player) then
+                system "cls"
+                puts ("<=============PLAYER #{@player}=============>")
+                puts ()
+                show
+                puts ()
+                return 1
+            end
+
+            @player = 2
+            if turn(@player) then
+                system "cls"
+                puts ("<=============PLAYER #{@player}=============>")
+                puts ()
+                show
+                puts ()
+                return 2
+            end
+
+        end
+    end
 end
 
 # create a class object for TicTacToe and call the game method
@@ -132,9 +170,9 @@ game = TicTacToe.new
 result = game.game
 
 if result == 0 then
-	puts ('The game ends in a tie')
+    puts ('The game ends in a tie')
 elsif result == 1
-	puts ("Player 1 wins!")
+    puts ("Player 1 wins!")
 else
-	puts ("Player 2 wins!")
+    puts ("Player 2 wins!")
 end
