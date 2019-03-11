0b1e0540342a0a1cca958f6f11915650ff141031
tackyunicorn <joshjoseph97@gmail.com>
2019-03-11 14:00:15 +0900

    :dart: allowing for user input of size

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -10,35 +10,38 @@ class Matrix
 end
 
 class TicTacToe
+    attr_accessor :size
 
-    # set the default size to be 3 x 3
-    @@size = 4
+    def init(params = {})
+        # set the default size to be 3 x 3
+        @@size = params.fetch(:size , 3)
 
-    # used to initialize the spots for the win configurations
-    @@spot = 0
+        # used to initialize the spots for the win configurations
+        @@spot = 0
 
-    # build a matrix of the specified size, initialize it with
-    # values of the spots, (1..size**2)
-    @@x = Matrix.build(@@size) {|row , col| @@spot += 1}
+        # build a matrix of the specified size, initialize it with
+        # values of the spots, (1..size**2)
+        @@x = Matrix.build(@@size) {|row , col| @@spot += 1}
 
-    # The board is a Hash of spots (keys) and their corresponding values
-    # (empty for now)
-    @@board = {}
-    (1..@@size**2).each {|i| @@board[i] = ' '}
+        # The board is a Hash of spots (keys) and their corresponding values
+        # (empty for now)
+        @@board = {}
+        (1..@@size**2).each {|i| @@board[i] = ' '}
 
-    # wins is a list containing all possible configurations of the
-    # board in the winning state
-    @@wins = Array.new()
-    @@temp = Array.new()
-    @@x.row_vectors.each do |sub|
-        @@temp.push(sub.to_a)
+        # wins is a list containing all possible configurations of the
+        # board in the winning state
+        @@wins = Array.new()
+        @@temp = Array.new()
+        @@x.row_vectors.each do |sub|
+            @@temp.push(sub.to_a)
+        end
+        @@temp.reverse.each { |x| @@wins.push(x) }
+        @@x.column_vectors.each do |sub|
+            @@wins.push(sub.to_a)
+        end
+        @@wins.push(@@x.extract_diagonal)
+        @@wins.push(@@x.rotate.extract_diagonal)
     end
-    @@temp.reverse.each { |x| @@wins.push(x) }
-    @@x.column_vectors.each do |sub|
-        @@wins.push(sub.to_a)
-    end
-    @@wins.push(@@x.extract_diagonal)
-    @@wins.push(@@x.rotate.extract_diagonal)
 
     # show is a function that displays the current state of the board
     def show()
@@ -65,7 +68,7 @@ class TicTacToe
     end
 
     # checkline is used as a helper function for check_all that returns
-    # the move if all the spots in a line have the same move
+    # the true if all the spots in a line have the same move
     def checkline(mark , move , config)
         (0...@@wins[config].length).each do |i|
             if @@board[@@wins[config][i]] != mark then
@@ -75,8 +78,8 @@ class TicTacToe
         return true
     end
 
-    # check_all checks the board for all possible win configurations on a 
-    # particular move and returns true if a configuration is found
+    # check_all checks the board for the win configurations of the particular
+    # move and retuns true if chekline returns true
     def check_all(mark , move)
         (0...@@wins.length).each do |i|
             if @@wins[i].include?(move) then
@@ -128,13 +131,26 @@ class TicTacToe
 
     # game starts the game and keeps a counter of spots taken
     # to check for a tie condition. returns 0 on a tie, 1 on
-    # player 1 winning and 2 on player 2 winning
+    # player 1 winning and 2 on player 2 winning. the minumum
+    # size of the board is 3
     def game()
+
+        system "cls"
+        puts "Enter the size of the tic-tac-toe board: "
+        size = gets.chomp.to_i
+        if (size > 2) then
+            init(:size => size)
+        else
+            puts("Invalid input")
+            sleep(1)
+            game()
+        end
+        
         @taken = 0
 
         while true do
 
-            if (@taken > @@size**2-2) then
+            if (@taken >= (@@size**2)-2) then
                 system "cls"
                 puts ("<=============PLAYER #{@player}=============>")
                 puts ()
